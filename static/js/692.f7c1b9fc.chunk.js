66bfe7f1d726edf6cce16a7ed41d51f27888e8db
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-12-30 11:51:11 +0200

    Updates

---
+++ static/js/692.f7c1b9fc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkproxy_band=self.webpackChunkproxy_band||[]).push([[692],{7692:function(t,n,a){a.d(n,{YiX:function(){return u}});var r=a(9983);function u(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M21 11H6.414l5.293-5.293-1.414-1.414L2.586 12l7.707 7.707 1.414-1.414L6.414 13H21z"}}]})(t)}}}]);
+//# sourceMappingURL=692.f7c1b9fc.chunk.js.map
